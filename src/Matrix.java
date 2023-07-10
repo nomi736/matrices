107ea9d141294ffac41b327340b9d07e393cf5f8
noumanalisyed <nouman.ali.syed@gmail.com>
2023-07-10 14:59:50 +0500

    implmented the Matrix addition function

--- src/Matrix.java
+++ src/Matrix.java
@@ -19,4 +19,22 @@ public class Matrix {
             System.out.print("\n");
         }
     }
+
+    public Matrix add(Matrix matrix){
+        Matrix result;
+        if(this.row == matrix.row && this.col == matrix.col){
+            result = new Matrix(row,col);
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
+                    result.data[i][j] = this.data[i][j] + matrix.data[i][j];
+                }
+            }
+
+        }
+        else{
+            System.out.println("Matrix -- Addition of two matrices " +
+                    "is not possible because there order is not same");
+        }
+        return result;
+    }
 }
